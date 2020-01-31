13dbca219e6d211342fcc7a20e29e4cd170bbf4c
Kathryn Ellington <Kathryn.Ellington@ibm.com>
2020-01-31 16:12:53 +0000

    Update commands

--- src/index.js
+++ src/index.js
@@ -6,24 +6,38 @@ const app = express();
 const port = process.env.PORT || "8080";
 
 const filePaths = [
-  { name: "bp-con-7min-demo", path: "/boilerplate-concurrency-7min-demo.jmx" },
-  { name: "bp-con-demo", path: "/boilerplate-concurrency-demo.jmx" },
-  { name: "bp-db", path: "/boilerplate-db.jmx" },
-  { name: "bp-spike-demo", path: "/boilerplate-spike-demo.jmx" },
-  { name: "boilerplate", path: "/boilerplate.jmx" },
-  { name: "cloudssky", path: "/cloudssky.jmx" },
-  { name: "defra", path: "/defra.jmx" },
-  { name: "graphresults", path: "/graphresults.jmx" },
+  {
+    name: "bp-con-7min-demo",
+    path: "/jmeter/boilerplate-concurrency-7min-demo.jmx"
+  },
+  { name: "bp-con-demo", path: "/jmeter/boilerplate-concurrency-demo.jmx" },
+  { name: "bp-db", path: "/jmeter/boilerplate-db.jmx" },
+  { name: "bp-spike-demo", path: "/jmeter/boilerplate-spike-demo.jmx" },
+  { name: "boilerplate", path: "/jmeter/boilerplate.jmx" },
+  { name: "cloudssky", path: "/jmeter/cloudssky.jmx" },
+  { name: "defra", path: "/jmeter/defra.jmx" },
+  { name: "graphresults", path: "/jmeter/graphresults.jmx" },
   { name: "test", path: "/jmeter/testScript.jmx" }
 ];
 
 app.get("/file/:fileId", (req, res) => {
   const filePath = filePaths.find(x => x.name === req.params.fileId).path;
   console.log(filePath);
-
+  let master_pod = "";
   exec(
-    "oc get pods",
-    // `oc exec -ti $master_pod -- /bin/bash /jmeter/load_test ${filePath}`,
+    `oc get pod  | grep jmeter-master | awk '{print $1}'`,
+    (error, stdout, sterr) => {
+      console.log(stdout);
+      master_pod = stdout;
+      console.log(stderr);
+      if (error !== null) {
+        console.log(`exec error: ${error}`);
+      }
+    }
+  );
+  exec(
+    // "oc get pods",
+    `oc exec -ti ${master_pod} -- /bin/bash /jmeter/load_test ${filePath}`,
     (error, stdout, stderr) => {
       console.log(stdout);
       console.log(stderr);
